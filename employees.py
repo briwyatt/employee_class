339f721199489c9732f12647f294ee61d2743bb8
Bri Wyatt <mbriwyatt@gmail.com>
2017-04-08 21:53:15 -0500

    first commit

---
+++ employees.py
@@ -0,0 +1,17 @@
+class Company(object):
+
+    def __init__(self, name, title, start_date):
+        self.name = name
+        self.title = title
+        self.start_date = start_date
+
+    def getName(self):
+        return self.name
+    
+    # Add the remaining methods to fill the requirements above\
+
+    def getTitle(self):
+        return self.title
+
+    def getStart_Date(self):
+        return self.start_Date
